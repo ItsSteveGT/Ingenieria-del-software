8b2ccc927c669109f3be99e7b4b88f5261bebcbf
Ervin Steve Melgar Alvarado <stevemelgat@gmail.com>
2025-05-13 15:53:53 -0600

    Prueba erroes completado

--- test.py
+++ test.py
@@ -10,19 +10,19 @@ class TestJuego(unittest.TestCase):
         self.assertEqual(validacion("piedra", "papel"), -1)
 
     def test_piedra_vs_piedra(self):
-        self.assertEqual(validacion("piedra", "piedra"), 5)
+        self.assertEqual(validacion("piedra", "piedra"), 0)
 
     def test_papel_vs_piedra(self):
         self.assertEqual(validacion("papel", "piedra"), 1)
 
     def test_papel_vs_tijera(self):
-        self.assertEqual(validacion("papel", "tijera"), -3)
+        self.assertEqual(validacion("papel", "tijera"), -1)
 
     def test_papel_vs_papel(self):
         self.assertEqual(validacion("papel", "papel"), 0)
 
     def test_tijera_vs_papel(self):
-        self.assertEqual(validacion("tijera", "papel"), 2)
+        self.assertEqual(validacion("tijera", "papel"), 1)
 
     def test_tijera_vs_piedra(self):
         self.assertEqual(validacion("tijera", "piedra"), -1)
